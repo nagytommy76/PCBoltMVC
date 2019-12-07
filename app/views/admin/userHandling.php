411048c27c7abaab28c8a6cb69dbd3eca50bf9ed
nagytommy76 <45012994+nagytommy76@users.noreply.github.com>
2019-12-07 17:50:16 +0100

    Add files via upload

--- app/views/admin/userHandling.php
+++ app/views/admin/userHandling.php
@@ -7,6 +7,7 @@
                 flash('deleted');
             ?>
         </div>
+        <h2>Felhasználók akik kitöltötték az adatokat: </h2>
         <table class="table table-hover table-sm bg-warning mb-5 ">
             <thead>
                 <tr>
@@ -36,5 +37,32 @@
                 <?php endforeach; ?>
             </tbody>
         </table>  
+        <h2 class="text-warning">Felhasználók akik <span class="text-danger">NEM</span> töltötték ki az adatokat: </h2>
+
+        <table class="table table-hover table-sm bg-warning mb-5 ">
+            <thead>
+                <tr>
+                    <th scope="col">Művelet: </th>
+                    <th scope="col">Email: </th>
+                    <th scope="col">Felhasználó: </th>                                             
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($data['noDataUsers'] as $user) : ?>
+                <tr class="table-warning">
+                    <td>
+                        <div class="form-row">
+                            <form action="<?php echo URLROOT; ?>/admins/deleteUser/<?php echo $user->email; ?>/<?php echo $user->username; ?>" method="POST">
+                                <input type="submit" value="Törlés" class="btn btn-danger btn-sm">
+                            </form>
+                        </div>
+                    </td>
+                    <td><?php echo $user->email; ?></td>
+                    <td><?php echo $user->username; ?></td>                                    
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>  
+
     
 <?php require FOOTER; ?>
