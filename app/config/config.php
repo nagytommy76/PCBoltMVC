00a5d6675e8cf02265c2667f58ef68fb14cc62c4
nagytommy76 <45012994+nagytommy76@users.noreply.github.com>
2020-01-19 17:33:09 +0100

    Add files via upload

--- app/config/config.php
+++ app/config/config.php
@@ -9,13 +9,16 @@
     define('APPROOT',dirname(dirname(__FILE__)));
 
     // URL Root
-    //define('URLROOT', 'http://localhost/PCBoltMVC');
-    define('URLROOT', 'https://nagytamasweboldal.herokuapp.com/');
+    define('URLROOT', 'http://localhost/PCBoltMVC');
+    //define('URLROOT', 'https://nagytamasweboldal.000webhostapp.com');
 
     // Site name
     define('SITENAME', 'Computer Store |');
 
     // Ikonok elérési útvonala:
+    /***
+     * http://localhost/PCBoltMVC/img....
+     */
     define('ICONROOT', URLROOT.'/img');
 
     // FOOTER
